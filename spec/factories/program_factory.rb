7d8d799bb69f7bd62612688830ee8e4156988096
Albert Callarisa Roca <shark234@gmail.com>
2012-09-11 21:04:48 +0800

    Fixed build and stubbed Pusher in tests

--- spec/factories/program_factory.rb
+++ spec/factories/program_factory.rb
@@ -4,13 +4,6 @@ FactoryGirl.define do
       "Company #{n}"
     end
     genre 'action'
-
-    after(:build) do |program|
-      unless program.company
-        user = create(:user)
-        program.company = user.company
-      end
-    end
   end
 
   factory(:movie, class: "Movie", parent: :program)
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,4 +14,8 @@ RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
 
   config.order = "random"
+
+  config.before do
+    Pusher.stub(:[]) { double.as_null_object }
+  end
 end
