732d1bded589473581afa64a2f03fd29d4be4f32
Albert Callarisa Roca <shark234@gmail.com>
2012-09-11 08:00:49 +0800

    Added Pusher test environment access by default

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,4 +1,4 @@
-Pusher.app_id = ENV["PUSHER_APP_ID"]
-Pusher.key = ENV["PUSHER_KEY"]
-Pusher.secret = ENV["PUSHER_SECRET"]
+Pusher.app_id = ENV["PUSHER_APP_ID"] || "27473"
+Pusher.key = ENV["PUSHER_KEY"] || "16c64c90d48269b8f5aa"
+Pusher.secret = ENV["PUSHER_SECRET"] || "a811f7a302957c116592"
 Pusher.logger = Rails.logger
